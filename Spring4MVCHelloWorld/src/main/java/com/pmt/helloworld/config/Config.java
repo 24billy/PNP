262d1b71d6e0eac2d6cd28da2a7f12bcee9cf211
24billy <deadbilly04@gmail.com>
2014-12-24 16:11:52 +0800

    modified coding style

--- Spring4MVCHelloWorld/src/main/java/com/pmt/helloworld/config/Config.java
+++ Spring4MVCHelloWorld/src/main/java/com/pmt/helloworld/config/Config.java
@@ -1,13 +1,13 @@
 /**
- * Copyright (c) 2014 Far Eastone Telecommunications Co., Ltd.
+ * Copyright (c) 2014 Pro Meritage Inte'l Co., Ltd.
  * All Rights Reserved.
  *
- * This software is the confidential and proprietary information of
- * Far Eastone Telecommunications Co., Ltd. ("Confidential Information").
- *
- * You shall not disclose such Confidential Information and shall use it
+ * This software is the confidential and proprietary information of 
+ * Pro Meritage Inte'l Co., Ltd. ("Confidential Information"). 
+ * 
+ * You shall not disclose such Confidential Information and shall use it 
  * only in accordance with the terms of license agreement you entered
- * into with Far Eastone Telecommunications Co., Ltd.
+ * into with Pro Meritage Inte'l Co., Ltd.
  */
 
 package com.pmt.helloworld.config;
@@ -20,9 +20,9 @@ import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 /**
- * 取代 dispatcher-servlet.xml
  * 
  * @author Billy
+ * replace  dispatcher-servlet.xml
  */
 @Configuration
 // Marks this class as configuration
--- Spring4MVCHelloWorld/src/main/java/com/pmt/helloworld/controller/HelloWorldController.java
+++ Spring4MVCHelloWorld/src/main/java/com/pmt/helloworld/controller/HelloWorldController.java
@@ -1,13 +1,13 @@
 /**
- * Copyright (c) 2014 Far Eastone Telecommunications Co., Ltd.
+ * Copyright (c) 2014 Pro Meritage Inte'l Co., Ltd.
  * All Rights Reserved.
  *
- * This software is the confidential and proprietary information of
- * Far Eastone Telecommunications Co., Ltd. ("Confidential Information").
- *
- * You shall not disclose such Confidential Information and shall use it
+ * This software is the confidential and proprietary information of 
+ * Pro Meritage Inte'l Co., Ltd. ("Confidential Information"). 
+ * 
+ * You shall not disclose such Confidential Information and shall use it 
  * only in accordance with the terms of license agreement you entered
- * into with Far Eastone Telecommunications Co., Ltd.
+ * into with Pro Meritage Inte'l Co., Ltd.
  */
 
 package com.pmt.helloworld.controller;
@@ -27,9 +27,10 @@ public class HelloWorldController {
     @RequestMapping("/hello")
     public String hello(
             @RequestParam(value = "name", required = false, defaultValue = "World") String name,
-            Model model) {
+            Model model) 
+    {
         model.addAttribute("name", name);
-        
+
         return "helloworld";
     }
 
