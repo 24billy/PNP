d7b630a379b37c284383d90b72154e0e7a9693c0
24billy <deadbilly04@gmail.com>
2014-12-24 15:28:26 +0800

    move files to specific folder

---
+++ Spring4MVCHelloWorld/src/main/java/com/pmt/helloworld/config/WebInitializer.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2014 Far Eastone Telecommunications Co., Ltd.
+ * All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Far Eastone Telecommunications Co., Ltd. ("Confidential Information").
+ *
+ * You shall not disclose such Confidential Information and shall use it
+ * only in accordance with the terms of license agreement you entered
+ * into with Far Eastone Telecommunications Co., Ltd.
+ */
+
+package com.pmt.helloworld.config;
+
+
+ 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import com.pmt.helloworld.controller.*;
+/**
+ *
+ * @author Billy
+ */
+public class WebInitializer implements WebApplicationInitializer {
+
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(Config.class);
+        
+        ctx.setServletContext(servletContext);
+        
+        Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+        servlet.addMapping("/");
+        servlet.setLoadOnStartup(1);
+        
+        }
+}
